7fffc6c08071336928f588d72041a6c53f03559f
User91510 <122837346+User91510@users.noreply.github.com>
2024-11-25 12:29:41 -0800

    Basic components

---
+++ my-app/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/index.html',
+    './src/Components/Header.tsx',
+    './src/**/*.{html,js,tsx,jsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
